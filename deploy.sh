72d7487be580ba6a34677555964050212216da41
Ezequiel Reyno <ezequiel.reyno@gmail.com>
2018-03-31 13:59:10 -0300

    Add right path to nuget

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 ApiKey=$1
 
-./.nuget/NuGet.exe pack ./log4net.Appenders.nuspec -Verbosity detailed
+/home/travis/build/skielo/log4net.Appenders/.nuget/NuGet.exe pack ./log4net.Appenders.nuspec -Verbosity detailed
 
-./.nuget/NuGet.exe push ./log4net.Appenders.*.nupkg -Verbosity detailed -ApiKey $ApiKey
+/home/travis/build/skielo/log4net.Appenders/.nuget/NuGet.exe push ./log4net.Appenders.*.nupkg -Verbosity detailed -ApiKey $ApiKey
