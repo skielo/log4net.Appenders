fc5841051cbc40783ede6b67c83b48adf602c44a
Ezequiel Reyno <ezequiel.reyno@gmail.com>
2018-03-31 13:38:46 -0300

    use the local version of nuget app

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 ApiKey=$1
 
-nuget pack ./log4net.Appenders.nuspec -Verbosity detailed
+./.nuget/nuget pack ./log4net.Appenders.nuspec -Verbosity detailed
 
-nuget push ./log4net.Appenders.*.nupkg -Verbosity detailed -ApiKey $ApiKey
+./.nuget/nuget push ./log4net.Appenders.*.nupkg -Verbosity detailed -ApiKey $ApiKey
