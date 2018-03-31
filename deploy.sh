c8fc6e901e5b5b3ed6a30ef83457b9add6219520
Ezequiel Reyno <ezequiel.reyno@gmail.com>
2018-03-31 13:54:35 -0300

    Try to push to nuget

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 ApiKey=$1
 
-./.nuget/nuget pack ./log4net.Appenders.nuspec -Verbosity detailed
+./.nuget/NuGet.exe pack ./log4net.Appenders.nuspec -Verbosity detailed
 
-./.nuget/nuget push ./log4net.Appenders.*.nupkg -Verbosity detailed -ApiKey $ApiKey
+./.nuget/NuGet.exe push ./log4net.Appenders.*.nupkg -Verbosity detailed -ApiKey $ApiKey
