30424c535d1bbf86b1be4e2850a9b0ba293c43e2
Ezequiel Reyno <ezequiel.reyno@gmail.com>
2018-05-03 10:14:12 -0300

    Group into batches by partition key. Improve comments on summary.

--- log4net.Azure/AzureTableAppender.cs
+++ log4net.Azure/AzureTableAppender.cs
@@ -8,16 +8,23 @@ using System.Linq;
 
 namespace log4net.Appender
 {
+    /// <summary>
+    /// Azure Table appender for log4net
+    /// </summary>
     public class AzureTableAppender : BufferingAppenderSkeleton
     {
         private CloudStorageAccount _account;
         private CloudTableClient _client;
         private CloudTable _table;
-
+        /// <summary>
+        /// Connection string name
+        /// </summary>
         public string ConnectionStringName { get; set; }
 
         private string _connectionString;
-
+        /// <summary>
+        /// Connection string of the Azure Table
+        /// </summary>
         public string ConnectionString
         {
             get
@@ -38,7 +45,9 @@ namespace log4net.Appender
 
 
         private string _tableName;
-
+        /// <summary>
+        /// Table name to store events
+        /// </summary>
 	    public string TableName
         {
             get
@@ -52,26 +61,35 @@ namespace log4net.Appender
                 _tableName = value;
             }
         }
-
+        /// <summary>
+        /// Properties as Colummns
+        /// </summary>
         public bool PropAsColumn { get; set; }
 
 	    private PartitionKeyTypeEnum _partitionKeyType = PartitionKeyTypeEnum.LoggerName;
+        /// <summary>
+        /// Enumeration for different partition key types
+        /// </summary>
         public PartitionKeyTypeEnum PartitionKeyType
         {
             get { return _partitionKeyType; }
             set { _partitionKeyType = value; }
         }
 
+        /// <summary>
+        /// Push events to Azure Table
+        /// </summary>
+        /// <param name="events">Buffered events in log4net</param>
         protected override void SendBuffer(LoggingEvent[] events)
         {
-            var grouped = events.GroupBy(evt => evt.LoggerName);
+            var grouped = events.Select(GetLogEntity).GroupBy(evt => evt.PartitionKey);
 
             foreach (var group in grouped)
             {
                 foreach (var batch in group.Batch(100))
                 {
                     var batchOperation = new TableBatchOperation();
-                    foreach (var azureLoggingEvent in batch.Select(GetLogEntity))
+                    foreach (var azureLoggingEvent in batch)
                     {
                         batchOperation.Insert(azureLoggingEvent);
                     }
@@ -92,6 +110,9 @@ namespace log4net.Appender
                 : new AzureLoggingEventEntity(@event, PartitionKeyType);
         }
 
+        /// <summary>
+        /// Activate options for log4net
+        /// </summary>
         public override void ActivateOptions()
         {
             base.ActivateOptions();
